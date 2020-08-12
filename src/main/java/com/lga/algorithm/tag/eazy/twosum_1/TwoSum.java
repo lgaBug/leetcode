5ac8d6207579b37de6074a3b65ba0341e6fd3ad6
lgaBug <18270234907@163.com>
2020-08-12 14:38:15 +0800

    update formate

--- src/main/java/com/lga/algorithm/tag/eazy/twosum_1/TwoSum.java
+++ src/main/java/com/lga/algorithm/tag/eazy/twosum_1/TwoSum.java
@@ -10,26 +10,26 @@ import java.util.Map;
 
 /**
  * 1. 两数之和
- *
+ * <p>
  * subject description:
  * 给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那 两个 整数，并返回他们的数组下标。
- *
+ * <p>
  * 你可以假设每种输入只会对应一个答案。但是，数组中同一个元素不能使用两遍。
- *
+ * <p>
  * 来源：力扣（LeetCode）
  * 链接：https://leetcode-cn.com/problems/two-sum
- *
+ * <p>
  * case 1:
  * 给定 nums = [2, 7, 11, 15], target = 9
- *
+ * <p>
  * 因为 nums[0] + nums[1] = 2 + 7 = 9
  * 所以返回 [0, 1]
- *
  */
 public class TwoSum {
 
     /**
      * 一遍哈希
+     *
      * @param nums
      * @param target
      * @return
@@ -39,20 +39,20 @@ public class TwoSum {
         for (int i = 0; i < nums.length; i++) {
             int oneVal = target - nums[i];
             if (map.containsKey(oneVal)) {
-                return new int[]{map.get(oneVal),i};
+                return new int[]{map.get(oneVal), i};
             }
             map.put(nums[i], i);
         }
         return null;
     }
 
-        /**
-         *
-         * 两遍哈希
-         * @param nums
-         * @param target
-         * @return
-         */
+    /**
+     * 两遍哈希
+     *
+     * @param nums
+     * @param target
+     * @return
+     */
     public int[] twoSum2(int[] nums, int target) {
         Map<Integer, Integer> map = new HashMap<Integer, Integer>();
         for (int i = 0; i < nums.length; i++) {
@@ -67,7 +67,13 @@ public class TwoSum {
         return null;
     }
 
-        public int[] twoSum1(int[] nums, int target) {
+    /**
+     * think error
+     * @param nums
+     * @param target
+     * @return
+     */
+    public int[] twoSum1(int[] nums, int target) {
         int[] ans = null;
         if (nums == null || nums.length < 2) return ans;
         //排序
@@ -93,17 +99,17 @@ public class TwoSum {
 
     @Test
     public void test1() {
-       Assert.assertTrue(Arrays.equals(new int[]{0,1},twoSum(new int[]{2, 7, 11, 15}, 9)));
+        Assert.assertTrue(Arrays.equals(new int[]{0, 1}, twoSum(new int[]{2, 7, 11, 15}, 9)));
     }
 
     @Test
     public void test2() {
-        Assert.assertTrue(Arrays.equals(new int[]{1,2},twoSum(new int[]{3, 2, 4}, 6)));
+        Assert.assertTrue(Arrays.equals(new int[]{1, 2}, twoSum(new int[]{3, 2, 4}, 6)));
     }
 
     @Test
     public void test3() {
-        Assert.assertTrue(Arrays.equals(new int[]{1,2},twoSum(new int[]{2, 5, 5,11}, 10)));
+        Assert.assertTrue(Arrays.equals(new int[]{1, 2}, twoSum(new int[]{2, 5, 5, 11}, 10)));
     }
 
 }
