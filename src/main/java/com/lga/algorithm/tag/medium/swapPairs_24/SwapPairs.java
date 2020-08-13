5f5b68aacf52dd2cf2381b0c963fbc7a321462de
lgaBug <18270234907@163.com>
2020-08-13 13:22:17 +0800

    24. 两两交换链表中的节点 遍历法

--- src/main/java/com/lga/algorithm/tag/medium/swapPairs_24/SwapPairs.java
+++ src/main/java/com/lga/algorithm/tag/medium/swapPairs_24/SwapPairs.java
@@ -1,6 +1,7 @@
 package com.lga.algorithm.tag.medium.swapPairs_24;
 
 import com.lga.datastruct.lru.ListNode;
+import org.junit.Test;
 
 /**
  * 24. 两两交换链表中的节点
@@ -23,8 +24,59 @@ public class SwapPairs {
         ListNode secondNode = head.next;
         firstNode.next = swapPairs(secondNode.next);
         secondNode.next = firstNode;
-
         return secondNode;
+    }
+
+    /**
+     * 递归法
+     * @param head
+     * @return
+     */
+    public ListNode swapPairs_1(ListNode head) {
+        if (head == null || head.next ==null) return head;
+        ListNode first = head;
+        ListNode second = head.next;
+        first.next = swapPairs_1(second.next);
+        second.next = first;
+        return second;
+    }
+
+    /**
+     * 遍历法
+     */
+    public ListNode swapPairs_2(ListNode head) {
+        ListNode dummy = new ListNode(-1);
+        dummy.next = head;
+        ListNode prevNode = dummy;
+
+        while (head != null && head.next != null) {
+            ListNode first = head;
+            ListNode second = head.next;
+
+            first.next = head.next.next;
+            second.next = first;
+            prevNode.next = second;
+            prevNode = first;
+            head = first.next;
+        }
+        return dummy.next;
+    }
+
+    @Test
+    public void test1() {
+        ListNode listNode1 = new ListNode(1);
+        ListNode listNode2 = new ListNode(2);
+        ListNode listNode3 = new ListNode(3);
+        ListNode listNode4 = new ListNode(4);
+        listNode1.next = listNode2;
+        listNode2.next = listNode3;
+        listNode3.next = listNode4;
+
+        swapPairs_2(listNode1);
 
     }
+
+
+
+
 }
