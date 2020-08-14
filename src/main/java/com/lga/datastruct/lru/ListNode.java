ba2ef5bfe2e90e085fd2606229fec13b1179961a
lga <18270234907@163.com>
2020-08-14 23:25:03 +0800

    21. 合并两个有序链表 时间复杂度：O(n) 空间复杂度：O(1)

--- src/main/java/com/lga/datastruct/lru/ListNode.java
+++ src/main/java/com/lga/datastruct/lru/ListNode.java
@@ -4,6 +4,14 @@ public class ListNode {
     public int val;
     public ListNode next;
 
+    ListNode() {
+    }
+
+    public ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+
     public ListNode(int x) {
         val = x;
     }
