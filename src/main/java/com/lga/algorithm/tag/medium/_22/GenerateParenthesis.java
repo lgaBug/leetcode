c9c134dedbbf8d467bcd09ce5285f933f135f994
lga <18270234907@163.com>
2020-08-26 22:09:03 +0800

    22. 括号生成

--- src/main/java/com/lga/algorithm/tag/medium/_22/GenerateParenthesis.java
+++ src/main/java/com/lga/algorithm/tag/medium/_22/GenerateParenthesis.java
@@ -32,11 +32,40 @@ public class GenerateParenthesis {
             generate(left, right + 1, n, s + ")");
     }
 
+    /**
+     * 生成括号的所有可能
+     * @param n 括号对数
+     * @return
+     */
+    public void generateParenthesis_1(int n) {
+
+        generate_1(0, 2*n, "");
+
+    }
+
+    private void generate_1(int i, int n, String s) {
+
+        if (i >= n) {
+            System.out.println(s);
+            return;
+        }
+        generate_1(i+1,n, s + "(");
+        generate_1(i+1,n, s + ")");
+
+
+    }
+
     @Test
     public void test_generateParenthesis() {
 
         List<String> strings = generateParenthesis(3);
         System.out.println("strings = " + strings);
+    }
+
+
+    @Test
+    public void test_generateParenthesis_1() {
+        generateParenthesis_1(2);
     }
 
 }
