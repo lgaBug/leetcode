ced95935849db2268a7dceda2a2f25d0c76f42ac
lga <18270234907@163.com>
2020-08-27 00:21:29 +0800

    111. 二叉树的最小深度

--- src/main/java/com/lga/algorithm/tag/eazy/_104/MaxDepth.java
+++ src/main/java/com/lga/algorithm/tag/eazy/_104/MaxDepth.java
@@ -1,6 +1,7 @@
 package com.lga.algorithm.tag.eazy._104;
 
 import com.lga.datastruct.lru.TreeNode;
+import org.junit.Test;
 
 /**
  * 104. 二叉树的最大深度
@@ -16,7 +17,24 @@ public class MaxDepth {
         int right = maxDepth(root.right) + 1;
 
         return Math.max(left, right);
+    }
 
+
+    @Test
+    public void test() {
+
+        TreeNode root = new TreeNode(3);
+        TreeNode node1 = new TreeNode(9);
+        TreeNode node2= new TreeNode(20);
+        TreeNode node3 = new TreeNode(15);
+        TreeNode node4 = new TreeNode(7);
+
+        root.left = node1;
+        root.right = node2;
+        node2.left = node3;
+        node2.right = node4;
+
+        maxDepth(root);
 
     }
 }
