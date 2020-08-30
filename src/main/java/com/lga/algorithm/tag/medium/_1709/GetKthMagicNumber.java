588157a9e27b6f3bb424fbeb20204718d775f11c
lga <18270234907@163.com>
2020-08-30 23:24:21 +0800

    面试题 17.09. 第 k 个数 使用栈来解决的。但是复杂度过高，还需要进一步优化

---
+++ src/main/java/com/lga/algorithm/tag/medium/_1709/GetKthMagicNumber.java
@@ -0,0 +1,37 @@
+package com.lga.algorithm.tag.medium._1709;
+
+import java.util.HashSet;
+import java.util.PriorityQueue;
+import java.util.Queue;
+import java.util.Set;
+
+/**
+ * 面试题 17.09. 第 k 个数
+ * https://leetcode-cn.com/problems/get-kth-magic-number-lcci/
+ */
+public class GetKthMagicNumber {
+
+
+
+    public int getKthMagicNumber(int k) {
+        Set<Long> set = new HashSet<>();
+        Queue<Long> queue = new PriorityQueue<>();
+
+        queue.add(1L);
+
+        while (true) {
+            Long curMin = queue.poll();
+            if (!set.contains(curMin)) {
+                set.add(curMin);
+                queue.add(curMin * 3);
+                queue.add(curMin * 5);
+                queue.add(curMin * 7);
+            }
+
+            if (set.size() == k) {
+                return curMin.intValue();
+            }
+
+        }
+    }
+}
