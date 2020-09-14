5075171f84b35aaf8a1ce850b8df49d7d232d091
lgaBug <18270234907@163.com>
2020-09-14 13:36:05 +0800

    64. 最小路径和 递归+缓存、dp

--- src/main/java/com/lga/algorithm/tag/medium/_64/MinPathSum.java
+++ src/main/java/com/lga/algorithm/tag/medium/_64/MinPathSum.java
@@ -9,18 +9,19 @@ import org.junit.Test;
 public class MinPathSum {
 
     /**
-     * 傻递归
+     * 递归+cache
+     *
      * @param grid
      * @return
      */
     public int minPathSum(int[][] grid) {
         if (grid == null) return 0;
         int[][] arrs = new int[grid.length][grid[0].length];
-        opt(grid, grid.length - 1, grid[0].length - 1, arrs);
-        return arrs[grid.length - 1][grid[0].length - 1];
+        return opt(grid, grid.length - 1, grid[0].length - 1, arrs);
     }
 
     private int opt(int[][] grid, int i, int j, int[][] arrs) {
+        if (arrs[i][j] != 0) return arrs[i][j];
         if (i == 0 && j == 0) {
             return arrs[i][j] = grid[i][j];
         } else if (j == 0 && i != 0) {
@@ -32,9 +33,30 @@ public class MinPathSum {
         }
     }
 
+    /**
+     * dp 解法
+     *
+     * @param grid
+     * @return
+     */
+    public int minPathSum_dp(int[][] grid) {
+        if (grid == null) return 0;
+        int ylen = grid.length;
+        int xlen = grid[0].length;
+        for (int i = 0; i < ylen; i++) {
+            for (int j = 0; j < xlen; j++) {
+                if (i == 0) continue;
+                if (i == 0) grid[i][j] = grid[i][j - 1] + grid[i][j];
+                else if (j == 0 && i > 0) grid[i][j] = grid[i - 1][j] + grid[i][j];
+                else grid[i][j] = Math.min(grid[i][j - 1], grid[i - 1][j]) + grid[i][j];
+            }
+        }
+        return grid[ylen - 1][xlen - 1];
+    }
+
     @Test
     public void test() {
-        int ans = minPathSum(new int[][]{{1, 3, 1}, {1, 5, 1}, {4, 2, 1}});
+        int ans = minPathSum_dp(new int[][]{{1, 3, 1}, {1, 5, 1}, {4, 2, 1}});
         System.out.println("ans = " + ans);
     }
 }
