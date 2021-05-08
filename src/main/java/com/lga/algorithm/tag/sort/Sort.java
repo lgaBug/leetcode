4de4c0794b67d758cf980d09fcd2044b695fb183
gaoan.liu <18270234907@163.com>
2021-05-08 10:47:01 +0800

    【算法】 常见排序算法

--- src/main/java/com/lga/algorithm/tag/sort/Sort.java
+++ src/main/java/com/lga/algorithm/tag/sort/Sort.java
@@ -13,6 +13,28 @@ import java.util.List;
  */
 public class Sort {
 
+
+
+    // 冒泡排序，a表示数组，n表示数组大小
+    public void bubbleSort(int[] a, int n) {
+        if (n <= 1) return;
+
+        for (int i = 0; i < n; ++i) {
+            // 提前退出冒泡循环的标志位
+            boolean flag = false;
+            for (int j = 0; j < n - i - 1; ++j) {
+                if (a[j] > a[j+1]) { // 交换
+                    int tmp = a[j];
+                    a[j] = a[j+1];
+                    a[j+1] = tmp;
+                    flag = true;  // 表示有数据交换
+                }
+            }
+            if (!flag) break;  // 没有数据交换，提前退出
+        }
+    }
+
+
     /**
      * 插入排序,O(n^2) O(1) 原地稳定的排序算法
      *
@@ -144,6 +166,14 @@ public class Sort {
     public void test_quickSort() {
         int[] arr = new int[]{3, 2, 1, 6, 4, 5};
         quickSort(arr);
+        Assert.assertTrue(Arrays.equals(new int[]{1, 2, 3, 4, 5, 6}, arr));
+    }
+
+
+    @Test
+    public void test_bubbleSort() {
+        int[] arr = new int[]{3, 2, 1, 6, 4, 5};
+        bubbleSort(arr,arr.length);
         Assert.assertTrue(Arrays.equals(new int[]{1, 2, 3, 4, 5, 6}, arr));
     }
 
