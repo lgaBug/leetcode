dcc3a1f3519140842cbe5f27eab921195ffc675a
lgaBug <18270234907@163.com>
2021-08-01 19:29:32 +0800

    零矩阵

--- src/main/java/com/lga/algorithm/tag/arrayandstring/ArraysQuestion.java
+++ src/main/java/com/lga/algorithm/tag/arrayandstring/ArraysQuestion.java
@@ -65,4 +65,32 @@ public class ArraysQuestion {
         System.arraycopy(res,0,matrix,0,length);
     }
 
+    /**
+     * 零矩阵
+     * @param matrix
+     */
+    public void setZeroes(int[][] matrix) {
+
+        boolean[] row = new boolean[matrix.length];
+        boolean[] clomn = new boolean[matrix[0].length];
+
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                if (matrix[i][j] == 0) {
+                    row[i] = true;
+                    clomn[j] = true;
+                }
+            }
+        }
+
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                if (row[i] || clomn[j]) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+
+    }
+
 }
